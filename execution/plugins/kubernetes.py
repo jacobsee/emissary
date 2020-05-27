02a75bf8b2d4dd49c3b234159e0c2e6e6f9b6795
Jacob See <jacob.d.see@gmail.com>
2020-05-27 15:04:43 -0700

    Fix up some kubernetes plugin apply resource logic

--- execution/plugins/kubernetes.py
+++ execution/plugins/kubernetes.py
@@ -3,6 +3,8 @@ from os import listdir, path
 import yaml
 import re
 import inflect
+import urllib3
+
 
 def initialize(params):
     if "mode" not in params:
@@ -22,6 +24,16 @@ def load_config(params):
             config.load_incluster_config()
         except Exception as e:
             raise Exception(f"Unable to load in-cluster Kubernetes config: {e}")
+    elif params["mode"] == "kubeconfig":
+        try:
+            config.load_kube_config(params["kubeconfig"])
+            if "insecure" in params:
+                if params["insecure"]:
+                    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        except Exception as e:
+            raise Exception(f"Unable to load kubeconfig: {e}")
+    else:
+        raise Exception(f"{params['mode']} is not supported as a Kubernetes auth mechanism.")
 
 
 def apply_objects(context, params):
@@ -33,10 +45,11 @@ def apply_objects(context, params):
         for file in files:
             with open(path.abspath(file)) as f:
                 resource = yaml.load(f, Loader=yaml.FullLoader)
-                create_from_dict(client, resource, verbose=True, namespace=namespace)
+                print(f"Applying {file} to cluster")
+                apply_from_dict(client, resource, verbose=True, namespace=namespace)
 
 
-def create_from_dict(k8s_client, data, verbose=False, namespace='default', **kwargs):
+def apply_from_dict(k8s_client, data, verbose=False, namespace='default', **kwargs):
     api_exceptions = []
 
     if "List" in data["kind"]:
@@ -55,9 +68,8 @@ def create_from_dict(k8s_client, data, verbose=False, namespace='default', **kwa
             except client.rest.ApiException as api_exception:
                 api_exceptions.append(api_exception)
     else:
-        # This is a single object. Call the single item method
         try:
-            create_from_yaml_single_item(
+            apply_from_yaml_single_item(
                 k8s_client, data, verbose, namespace=namespace, **kwargs)
         except client.rest.ApiException as api_exception:
             api_exceptions.append(api_exception)
@@ -66,49 +78,90 @@ def create_from_dict(k8s_client, data, verbose=False, namespace='default', **kwa
         raise FailToCreateError(api_exceptions)
 
 
-def create_from_yaml_single_item(k8s_client, yml_object, verbose=False, namespace="", **kwargs):
+def apply_from_yaml_single_item(k8s_client, yml_object, verbose=False, namespace="", **kwargs):
     group, _, version = yml_object["apiVersion"].partition("/")
     if version == "":
         version = group
         group = "core"
     # Take care for the case e.g. api_type is "apiextensions.k8s.io"
     # Only replace the last instance
-    group = "".join(group.rsplit(".k8s.io", 1))
+    py_group = "".join(group.rsplit(".k8s.io", 1))
     # convert group name from DNS subdomain format to
     # python class name convention
-    group = "".join(word.capitalize() for word in group.split('.'))
-    fcn_to_call = "{0}{1}Api".format(group, version.capitalize())
-    try:
-        k8s_api = getattr(client, fcn_to_call)(k8s_client)
-    except:
-        k8s_api = client.CustomObjectsApi(k8s_client)
+    py_group = "".join(word.capitalize() for word in py_group.split('.'))
+    fcn_to_call = "{0}{1}Api".format(py_group, version.capitalize())
+
+    if hasattr(client, fcn_to_call):
+        k8s_api = getattr(client, fcn_to_call)()
+    else:
+        k8s_api = client.CustomObjectsApi()
+
     # Replace CamelCased action_type into snake_case
     kind = yml_object["kind"]
     kind = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', kind)
     kind = re.sub('([a-z0-9])([A-Z])', r'\1_\2', kind).lower()
-    # Expect the user to create namespaced objects more often
+
+    if "namespace" in yml_object["metadata"]:
+        namespace = yml_object["metadata"]["namespace"]
+        kwargs['namespace'] = namespace
+
     if hasattr(k8s_api, "create_namespaced_{0}".format(kind)):
-        # Decide which namespace we are going to put the object in,
-        # if any
-        # if "namespace" in yml_object["metadata"]:
-        #     namespace = yml_object["metadata"]["namespace"]
-        #     kwargs['namespace'] = namespace
-        resp = getattr(k8s_api, "create_namespaced_{0}".format(kind))(
-            body=yml_object, **kwargs)
+        try:
+            resp = getattr(k8s_api, "create_namespaced_{0}".format(kind))(
+                body=yml_object, namespace=namespace, **kwargs)
+        except Exception as e:
+            if "already exists" in str(e):
+                resp = getattr(k8s_api, "patch_namespaced_{0}".format(kind))(
+                    body=yml_object, namespace=namespace, name=yml_object["metadata"]["name"], **kwargs)
+            else:
+                raise e
+
     elif hasattr(k8s_api, "create_{0}".format(kind)):
         kwargs.pop('namespace', None)
-        resp = getattr(k8s_api, "create_{0}".format(kind))(
-            body=yml_object, **kwargs)
+        try:
+            resp = getattr(k8s_api, "create_{0}".format(kind))(
+                body=yml_object, **kwargs)
+        except Exception as e:
+            if "already exists" in str(e):
+                resp = getattr(k8s_api, "patch_{0}".format(kind))(
+                    body=yml_object, name=yml_object["metadata"]["name"], **kwargs)
+            else:
+                raise e
+
     else:
-        kwargs.pop('namespace', None)
-        resp = k8s_api.create_namespaced_custom_object(
-            group=group,
-            version=version,
-            namespace=namespace,
-            plural=inflect.engine().plural(kind).lower(),
-            body=yml_object, **kwargs)
+        try:
+            resp = k8s_api.create_namespaced_custom_object(
+                group=group,
+                version=version,
+                namespace=namespace,
+                plural=inflect.engine().plural(kind).lower(),
+                body=yml_object, **kwargs)
+        except Exception as e:
+            if "already exists" in str(e):
+                resp = k8s_api.patch_namespaced_custom_object(
+                    group=group,
+                    version=version,
+                    namespace=namespace,
+                    plural=inflect.engine().plural(kind).lower(),
+                    name=yml_object["metadata"]["name"],
+                    body=yml_object, **kwargs)
+            else:
+                raise e
     if verbose:
-        msg = "{0} created.".format(kind)
+        msg = "{0} applied".format(kind)
         if hasattr(resp, 'status'):
             msg += " status='{0}'".format(str(resp.status))
         print(msg)
+
+
+class FailToCreateError(Exception):
+
+    def __init__(self, api_exceptions):
+        self.api_exceptions = api_exceptions
+
+    def __str__(self):
+        msg = ""
+        for api_exception in self.api_exceptions:
+            msg += "Error from server ({0}): {1}".format(
+                api_exception.reason, api_exception.body)
+        return msg
